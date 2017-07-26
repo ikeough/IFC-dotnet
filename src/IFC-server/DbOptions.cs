a30ffb911b6ae08279020223803c6dd7d515fac5
Ian Keough <ian.keough@autodesk.com>
2017-07-26 12:15:38 -0700

    Move the server out of this repo.

--- src/IFC-server/DbOptions.cs
+++
@@ -1,7 +0,0 @@
-public class DbOptions
-{
-	public string EndPoint {get;set;}
-	public string Name{get;set;}
-	public string Collection{get;set;}
-	public string AuthKey{get;set;}
-}
--- src/IFC-server/README.md
+++
@@ -1,2 +0,0 @@
-# IFC-server
-A webserver based on ASP.NET Core and Entity Framework Core, which uses the IFC-dotnet library.
